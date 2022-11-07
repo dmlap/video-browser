a2941274ed924ef302e90ba73c0b44339f69d434
Dave LaPalomento <dlapalomento@gmail.com>
2022-11-07 13:34:20 +0000

    Workaround Virtual Keyboard Enter behavior The Virtual Keyboard on WebOS doesn't submit a single-element form like every other browser. Assume the broken behavior and submit on keydown until a keypress/submit shows up. On platforms that have normal Enter behavior, the workaround will disable itself after triggering the first form submission.

--- components/navigation.js
+++ components/navigation.js
@@ -7,6 +7,11 @@ import uniqueId from '../src/uniqueId'
 
 import styles from '../styles/Navigation.module.css'
 
+// feature-detection for Enter handling in forms
+// this is a one-time toggle so it's intentionally outside the react
+// lifecycle
+let emitsKeyPressOnEnter = false
+
 export default function Navigation (attributes) {
   const [id, setId] = useState()
   const [query, setQuery] = useState(attributes.query || '')
@@ -24,8 +29,32 @@ export default function Navigation (attributes) {
     setQuery(event.target.value)
   }
 
-  function search () {
+  function handleKeyPress (event) {
+    if (!emitsKeyPressOnEnter && event.code === 'Enter') {
+      // keypress event for the Enter key is firing normally so
+      // disable emulation
+      emitsKeyPressOnEnter = true
+      // this event was already emulated on keydown so short-circuit
+      // normal handling this time
+      // subsequent events will not be emulated
+      event.preventDefault()
+    }
+  }
+  function handleKeyDown (event) {
+    if (emitsKeyPressOnEnter || event.code !== 'Enter') {
+      return
+    }
+
+    // WebOS circa 2022 does not generate a keypress event for Enter
+    // on the virtual keyboard, or trigger form submission for single
+    // input forms. Trigger form submission on keydown for Enter until
+    // we see the correct behavior
+    return search({ preventDefault: () => {} })
+  }
+
+  function search (event) {
     event.preventDefault()
+
     if (query) {
       router.push({ path: 'search', pageProps: { query } })
     }
@@ -41,6 +70,8 @@ export default function Navigation (attributes) {
                      className={styles.input}
                      type="text"
                      onChange={handleChange}
+                     onKeyDown={handleKeyDown}
+                     onKeyPress={handleKeyPress}
                      value={query} />
             </form>
             <VLink path="home" className={styles.home}>Home</VLink>
