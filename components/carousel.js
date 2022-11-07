68160dd38449fcc713552bd4f7f4468c9415e5a3
Dave LaPalomento <dlapalomento@gmail.com>
2022-11-07 14:20:03 +0000

    Create a channel carousel Tweak the video carousel to display channels instead. Use it for the favorites display on the home screen.

--- components/carousel.js
+++ components/carousel.js
@@ -3,11 +3,30 @@ import Video from './video'
 
 import styles from '../styles/Carousel.module.css'
 
+export function ChannelCarousel ({ channels }) {
+  return (<ol className={styles.channels}>
+          {
+            channels.map((channel) => {
+              return (<li key={channel.feedUrl} className={styles.item}>
+                        <VLink path="channel" feedUrl={channel.feedUrl}>
+                          <picture>
+                            { channel.image && <source srcSet={channel.image} /> }
+                            <source srcSet="gray.gif" />
+                            <img src="gray0.png" alt="channel artwork" />
+                          </picture>
+                          {channel.title}
+                        </VLink>
+                      </li>)
+            })
+          }
+          </ol>)
+}
+
 export default function Carousel ({ videos }) {
   return (<ol className={styles.videos}>
               {
                 videos.map((video, ix) => {
-                  return (<li key={ix} className={styles.videoItem}>
+                  return (<li key={ix} className={styles.item}>
                           <VLink path="video" video={video}>
                             <picture>
                               { video.poster && <source srcSet={video.poster} /> }
--- components/home.js
+++ components/home.js
@@ -1,4 +1,5 @@
 import Carousel from './carousel'
+import { ChannelCarousel } from './carousel'
 import VLink from './vlink'
 import Channel from './channel'
 
@@ -15,21 +16,15 @@ export default function Home() {
   const favorites = useFavoritesStorage()
   const recents = useRecentsStorage()
 
-  const favoriteList = favorites.get().map((favorite) => {
-    return (<li key={favorite.feedUrl}>
-              <VLink path="channel" feedUrl={favorite.feedUrl}>
-                {favorite.title}
-              </VLink>
-            </li>)
-  })
-
   return (<main>
             <section>
               <h1>
                 Favorites
               </h1>
 
-              { favorites.get().length ? (<ul>{favoriteList}</ul>) : FAVORITES_HELP }
+              { favorites.get().length ?
+                (<ChannelCarousel channels={favorites.get()} />) : FAVORITES_HELP
+              }
             </section>
             { recents.ready && !!recents.get().length &&
               <section>
