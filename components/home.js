635848a613b923872cd32dcb0e208bd81044dabf
Anton Lantukh <alantukh@jwplayer.com>
2023-03-01 18:16:15 +0100

    Wizard

--- components/home.js
+++ components/home.js
@@ -1,43 +1,113 @@
+import { useRouter } from 'next/router';
+
 import Carousel from './carousel';
 import { ChannelCarousel } from './carousel';
 import VLink from './vlink';
 import Channel from './channel';
 import { useDNav } from './dnav';
 
-import { useFavoritesStorage, useRecentsStorage } from '../src/storage';
+import { useFavoritesStorage, useRecentsStorage, useWizardStorage } from '../src/storage';
+import { useITunesData } from '../pages/api/itunes';
+import { useYouTubeData } from '../pages/api/youtube';
+import Layout from '../components/layout';
 
 import styles from '../styles/Home.module.css';
 
 const FAVORITES_HELP = <small>{"Subscribe to channels you like and they'll show up here"}</small>;
 
-export default function Home() {
-  const favorites = useFavoritesStorage();
+const CATEGORIES = ['News', 'Sport', 'Comedy', 'Cars', 'JW Player'];
+
+function MainContent() {
   const recents = useRecentsStorage();
+  const favorites = useFavoritesStorage();
+  const wizardData = useWizardStorage();
 
   useDNav();
 
+  const { data: itunes } = useITunesData(wizardData.get().category);
+  const { data: youtube } = useYouTubeData(wizardData.get().category);
+
   return (
-    <main>
-      <section className={styles.hero}>
-        <h1>{"Clarkson's Farm 2"}</h1>
-        <p>
-          Follow Jeremy Clarkson as he attempts to run a farm in the countryside. With no previous farming experience,
-          Jeremy contends with the worst farming weather in decades, disobedient animals, unresponsive crops, and an
-          unexpected pandemic.
-        </p>
-      </section>
-      <section>
-        <h1>Favorites</h1>
-
-        {favorites.get().length ? <ChannelCarousel channels={favorites.get()} /> : FAVORITES_HELP}
-      </section>
-      {recents.ready && !!recents.get().length && (
-        <section>
-          <h1>Recents</h1>
-
-          <Carousel videos={Array.from(recents.get().slice(-8)).reverse()} />
+    <Layout>
+      <main>
+        <section className={styles.hero}>
+          <h1>Podcasts</h1>
+          {itunes && <ChannelCarousel channels={itunes?.results.map((result) => result.channel)} />}
+          <h1>YouTube</h1>
+          {youtube && <ChannelCarousel channels={youtube?.results.map((result) => result.channel)} />}
         </section>
-      )}
+        <section>
+          <h1>Favorites</h1>
+
+          {favorites.get().length ? <ChannelCarousel channels={favorites.get()} /> : FAVORITES_HELP}
+        </section>
+        {recents.ready && !!recents.get().length && (
+          <section>
+            <h1>Recents</h1>
+
+            <Carousel videos={Array.from(recents.get().slice(-8)).reverse()} />
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+function Wizard() {
+  const router = useRouter();
+  const wizardData = useWizardStorage();
+
+  useDNav();
+
+  const categoryHandler = (evt) => {
+    console.log(evt);
+    if (event.type === 'keyup' && event.code !== 'Enter') {
+      return;
+    }
+
+    const category = evt.target.dataset.category;
+
+    wizardData.set({ category });
+    router.replace({
+      query: { ...router.query, category: category.toLowerCase() },
+    });
+    router.reload();
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.wizard}>
+        <img className={styles.logo} src="logo.svg" alt="logo" />
+        <h1>Which category are you interested in?</h1>
+        <div className={styles.categories}>
+          {CATEGORIES.map((el) => (
+            <div
+              tabIndex="1"
+              data-category={el}
+              onClick={categoryHandler}
+              onKeyUp={categoryHandler}
+              className={styles.category}
+              key={el}
+            >
+              {el}
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
+
+export default function Home() {
+  const wizardData = useWizardStorage();
+
+  if (!wizardData.get()?.category) {
+    return <Wizard />;
+  }
+
+  return <MainContent />;
+}
+
+Home.getLayout = function (page) {
+  return page;
+};
--- src/storage.js
+++ src/storage.js
@@ -1,15 +1,37 @@
 import { useEffect, useState } from 'react';
 
-const EMPTY = Object.freeze([]);
+const EMPTY = {
+  WATCH_HISTORY: Object.freeze([]),
+  PREFERENCE: Object.freeze({}),
+};
 
 /**
  * Accepts a key String and returns a storage object to persist and
  * retrieve data. Objects are stored in localStorage and will persist
  * beyond the page load or browsing session.
  */
-function useStorage(key) {
+
+const setItem = (key, value) => {
+  const storageValue = JSON.stringify(value);
+
+  try {
+    window.localStorage.setItem(key, storageValue);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getItem = (key) => {
+  try {
+    return JSON.parse(window.localStorage.getItem(storageKey));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+function useStorage(key, empty) {
   const [ready, setReady] = useState(false);
-  const [values, setValues] = useState(EMPTY);
+  const [values, setValues] = useState(empty);
 
   useEffect(() => {
     const json = localStorage.getItem(key);
@@ -27,9 +49,9 @@ function useStorage(key) {
       }
     }
 
-    localStorage.setItem(key, '[]');
-    setValues(EMPTY);
-  }, [key]);
+    localStorage.setItem(key, Array.isArray(empty) ? '[]' : '{}');
+    setValues(empty);
+  }, [key, empty]);
 
   return {
     get: () => {
@@ -39,7 +61,7 @@ function useStorage(key) {
     ready,
 
     set: (newValues) => {
-      const frozen = Object.freeze(Array.from(newValues));
+      const frozen = Object.freeze(newValues);
       localStorage.setItem(key, JSON.stringify(frozen));
       setValues(frozen);
     },
@@ -47,11 +69,15 @@ function useStorage(key) {
 }
 
 function useFavoritesStorage() {
-  return useStorage('favorites');
+  return useStorage('favorites', EMPTY.WATCH_HISTORY);
 }
 
 function useRecentsStorage() {
-  return useStorage('recents');
+  return useStorage('recents', EMPTY.WATCH_HISTORY);
 }
 
-export { useFavoritesStorage, useRecentsStorage };
+function useWizardStorage() {
+  return useStorage('preference', EMPTY.PREFERENCE);
+}
+
+export { useFavoritesStorage, useRecentsStorage, useWizardStorage };
